77804b4581d5e79130075f1dbe93a6a083fa1203
Gustaf <gurreja@gmail.com>
2014-09-05 00:37:21 +0200

    Removed unnecessary comment

--- src/main/java/vswe/stevesfactory/util/Utils.java
+++ src/main/java/vswe/stevesfactory/util/Utils.java
@@ -2,9 +2,6 @@ package vswe.stevesfactory.util;
 
 import java.util.regex.Pattern;
 
-/**
- * Created by gustaf on 05/09/14.
- */
 public class Utils {
 
     private static final Pattern patternControlCode = Pattern.compile("(?i)\\u00A7[0-9A-FK-OR]");
